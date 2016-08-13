16a2e785fd6a9d9873e95dc9a864ac25ebadef1d
Garry <gurigoraya1@gmail.com>
2016-08-13 13:59:10 +0530

    User controller created

---
+++ src/Model/Table/UserTable.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * User Model
+ *
+ * @method \App\Model\Entity\User get($primaryKey, $options = [])
+ * @method \App\Model\Entity\User newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\User[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\User|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\User patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\User[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\User findOrCreate($search, callable $callback = null)
+ */
+class UserTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('user');
+        $this->displayField('ID');
+        $this->primaryKey('ID');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('ID')
+            ->allowEmpty('ID', 'create');
+
+        $validator
+            ->requirePresence('First_Name', 'create')
+            ->notEmpty('First_Name');
+
+        $validator
+            ->requirePresence('Last_Name', 'create')
+            ->notEmpty('Last_Name');
+
+        $validator
+            ->requirePresence('Email', 'create')
+            ->notEmpty('Email');
+
+        $validator
+            ->integer('Mobile')
+            ->requirePresence('Mobile', 'create')
+            ->notEmpty('Mobile');
+
+        $validator
+            ->requirePresence('City', 'create')
+            ->notEmpty('City');
+
+        $validator
+            ->requirePresence('Country', 'create')
+            ->notEmpty('Country');
+
+        return $validator;
+    }
+}
---
+++ tests/Fixture/UserFixture.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UserFixture
+ *
+ */
+class UserFixture extends TestFixture
+{
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    public $table = 'user';
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'ID' => ['type' => 'integer', 'length' => 7, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'First_Name' => ['type' => 'string', 'length' => 20, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'Last_Name' => ['type' => 'string', 'length' => 20, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'Email' => ['type' => 'string', 'length' => 20, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'Mobile' => ['type' => 'integer', 'length' => 20, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'City' => ['type' => 'string', 'length' => 20, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'Country' => ['type' => 'string', 'length' => 20, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['ID'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'latin1_swedish_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'ID' => 1,
+            'First_Name' => 'Lorem ipsum dolor ',
+            'Last_Name' => 'Lorem ipsum dolor ',
+            'Email' => 'Lorem ipsum dolor ',
+            'Mobile' => 1,
+            'City' => 'Lorem ipsum dolor ',
+            'Country' => 'Lorem ipsum dolor '
+        ],
+    ];
+}
